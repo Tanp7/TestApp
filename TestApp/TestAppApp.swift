e7ebc30cba2dc8dc0037d90060e152a351665462
Pz <tanp11210712@gmail.com>
2022-06-10 14:25:56 +0800

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by pz on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
